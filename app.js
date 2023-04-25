f45a013b6cbba6af1b0351346ca188df53d0cf80
Marc Schär <marc.schaer93@gmail.com>
2023-04-25 07:04:18 +0200

    second Commit

--- app.js
+++ app.js
@@ -85,3 +85,79 @@
 // mySome(['hello', 'mellow', 'cat', 'campfire', 'ella'], function(word){
 //   return word.value = 'hello';
 // })
+
+
+
+//Exercise 1
+
+const hasOddNumber = (array) => {
+  return array.some(function(num){
+    return (num % 2 !== 0)
+  })
+
+}
+
+hasOddNumber([1,2,2,2,2,2,4]) // true
+hasOddNumber([2,2,2,2,2,4]) // false
+
+// Exercise 2
+
+const hasAZero = (number) => {
+  const numberToString = number.toString().split('')
+  return numberToString.some(function(zero) {
+    return zero === '0';
+  })
+}
+
+hasAZero(33321232131012) // true
+hasAZero(1212121) // false
+
+// Exercise 3
+
+const hasOnlyOddNumbers = (array) => {
+  if(array.every((num)=>{return num % 2 !== 0})) return true 
+  return false;
+}
+
+
+hasOnlyOddNumbers([1,3,5,7]) // true
+hasOnlyOddNumbers([1,2,3,5,7]) // false
+
+// Ecercise 4
+
+const hasNoDuplicates = (array) => {
+  return array.every((num)=>{return array.indexOf(num) === array.lastIndexOf(num)})
+}
+
+hasNoDuplicates([1,2,3,1]) // false
+hasNoDuplicates([1,2,3]) // true
+
+
+// Ecercise 5
+
+const hasCertainKey = (arr, key) => {
+  return arr.every((object)=> {return key in object })
+}
+
+
+let arr = [
+  {title: "Instructor", first: 'Elie', last:"Schoppik"},
+  {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true},
+  {title: "Instructor", first: 'Matt', last:"Lane"},
+  {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
+]
+
+hasCertainKey(arr,'first') // true
+hasCertainKey(arr,'isCatOwner') // false
+
+// Exercise 6
+
+const hasCertainValue = (arr, key, value) => {
+  return arr.every((object)=> {return object[key] === value})
+}
+
+hasCertainValue(arr,'title','Instructor') // true
+hasCertainValue(arr,'first','Elie') // false
+
+
+
